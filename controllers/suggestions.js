132452d6a79c9f4fd95b824fa96407e175ed9fb5
MuriloEduardo <muriloeduardoooooo@gmail.com>
2023-05-17 11:38:41 -0300

    Remove try catch suggestions route

--- controllers/suggestions.js
+++ controllers/suggestions.js
@@ -3,20 +3,14 @@ const router = express.Router();
 const { chat } = require('../services/openia');
 
 router.get('/', async (req, res) => {
-    try {
-        const { email } = req.query;
+    const { email } = req.query;
 
-        const { choices: [{ message: { content } }] } = await chat(email, `
-            Sugira três coisas para esse usuário pedir à inteligência artificial, em no máximo três palavras.
-            Retorne a resposta em formato JSON desse jeito: [{"suggestion": "sugestão", "emoji": "emoji"}]
-        `);
+    const { choices: [{ message: { content } }] } = await chat(email, `
+        Sugira três coisas para esse usuário pedir à inteligência artificial, em no máximo três palavras.
+        Retorne a resposta em formato JSON desse jeito: [{"suggestion": "sugestão", "emoji": "emoji"}]
+    `);
 
-        res.json(content);
-    } catch (error) {
-        console.error(error, 'Suggestions Error');
-
-        res.status(500).json({ message: 'Internal server error' });
-    }
+    res.json(content);
 });
 
 module.exports = router;
