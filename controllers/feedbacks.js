cde58d42744629931507b3b286ba5011c6298e08
MuriloEduardo <muriloeduardoooooo@gmail.com>
2023-05-17 23:21:10 -0300

    Remove user table from feedbacks

--- controllers/feedbacks.js
+++ controllers/feedbacks.js
@@ -5,18 +5,13 @@ const router = express.Router();
 router.post('/', async (req, res) => {
     const { url, type, message, email } = req.body;
 
-    const users = db('users');
     const feedbacks = db('feedbacks');
-
-    const [user] = await users.where({ email });
-
-    if (!user) return res.sendStatus(400);
 
     await feedbacks.insert({
         url,
         type,
+        email,
         message,
-        user_id: user.id,
     });
 
     res.sendStatus(201);
